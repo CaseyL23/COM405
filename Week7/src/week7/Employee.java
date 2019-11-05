ff1b79afc8007f435c9b2349cdd39ec867b1959a
Casey Littlechild <4littc85@solent.ac.uk>
2019-11-05 15:08:35 +0000

    created classes

--- Week7/src/week7/Employee.java
+++ Week7/src/week7/Employee.java
@@ -3,21 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package week6;
+package week7;
 
 /**
  *
  * @author 4littc85
  */
-public class Week6 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args)
-    {
-       
-        
-    }
+public class Employee {
     
 }
--- Week7/src/week7/Week7.java
+++ Week7/src/week7/Week7.java
@@ -3,21 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package week6;
+package week7;
 
 /**
  *
  * @author 4littc85
  */
-public class Week6 {
+public class Week7 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
-    {
-       
-        
+    public static void main(String[] args) {
+        // TODO code application logic here
     }
     
 }

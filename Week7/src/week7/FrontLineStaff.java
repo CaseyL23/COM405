b0d4c3aca7ea3772ee9f79ac0e5be0df9b073894
Casey Littlechild <4littc85@solent.ac.uk>
2019-11-05 15:51:11 +0000

    further methods added to FrontLineStaff

--- Week7/src/week7/FrontLineStaff.java
+++ Week7/src/week7/FrontLineStaff.java
@@ -10,9 +10,18 @@ package week7;
  * @author 4littc85
  */
 public class FrontLineStaff extends Employee
-{
-    public FrontLineStaff(String NameIn, String payRateIn, double noContractedHrsIn, double HolidaysIn)
+  {      
+        protected int hourlyRate;
+
+        public FrontLineStaff(String NameIn, String payRateIn, double noContractedHrsIn, double HolidaysIn, int hourlyRateIn)
     {
         super (NameIn, payRateIn, noContractedHrsIn, HolidaysIn);
+        hourlyRate = hourlyRate;
     }
+
+        public int gethourlyRate ()
+{
+        return hourlyRate;
+}
+
 }

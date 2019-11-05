789c1da8280b0ffda39e38ecced8cd18a86424cd
Casey Littlechild <4littc85@solent.ac.uk>
2019-11-05 15:58:56 +0000

    methods added to Manager class

--- Week7/src/week7/Manager.java
+++ Week7/src/week7/Manager.java
@@ -9,6 +9,10 @@ package week7;
  *
  * @author 4littc85
  */
-public class Manager {
-    
+public class Manager extends Employee
+{
+  public Manager(String NameIn, String payRateIn, double noContractedHrsIn, double HolidaysIn, int hourlyRateIn)  
+  {
+      super (NameIn, payRateIn, noContractedHrsIn, HolidaysIn);
+  }
 }

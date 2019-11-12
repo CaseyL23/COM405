1e41c7049e895799dafc60aee6d7ac8404cbe294
Casey Littlechild <4littc85@solent.ac.uk>
2019-11-12 15:03:40 +0000

    Added all classes

---
+++ Week8/src/week8/Car.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week8;
+
+/**
+ *
+ * @author 4littc85
+ */
+public class Car {
+    private String reg;
+    
+    public Car(String regIn)
+    {
+        reg = regIn;
+    }
+    
+    public String getReg()
+    {
+        return reg;
+    }
+}

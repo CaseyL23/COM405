30093f44cf5c4750af40acc55d5b335542dc20b3
Casey Littlechild <4littc85@solent.ac.uk>
2019-11-12 17:13:10 +0000

    constraints

--- Week8/src/week8/Week8.java
+++ Week8/src/week8/Week8.java
@@ -12,55 +12,48 @@ package week8;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import.javax.swing.JTextField;
+import javax.swing.JTextField;
+
 
 public class Week8 extends JFrame 
     {
      
         private JLabel lblRegNo;
         private JLabel lblColour;
-        private JLabel lblMake;
-        private JLabel lblModel;
+        //private JLabel lblMake;
+        //private JLabel lblModel;
         private JLabel lblStatus;
         
         private JTextField txtRegNo;
         private JTextField txtColour;
-        private JTextField txtMake;
-        private JTextField txtModel;
+       // private JTextField txtMake;
+       // private JTextField txtModel;
         
         private JButton btnAdd;
         private JButton btnRemove;
-        private Object constraints;
+        private GridBagConstraints constraints;
         
     
-public Week8 () {
+    public Week8 () {
     
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new GridBagLayout());
+        constraints = new GridBagConstraints();
         initComponents();
 
-    /**
-     *
-     */                            
-    public void initComponents();
+    
+    public void initComponents()
         {
-        lblRegNo = new JLabel("Registration No:");
-        lblColour = new JLabel("Colour:");
-        lblMake = new JLabel("Make:");
-        lblModel = new JLabel("Model:");
-        lblStatus = new JLabel ("Car Park has 15 empty spaces");
-        txtRegNo = new JTextField ();
-        txtColour = new JTextField ();
-        txtMake = new JTextField ();
-        txtModel = new JTextField ();
-        btnAdd = new JButton("Add Car");
-        btnRemove = new JButton("Remove Car");
+            lblRegNo = new JLabel("Registration No:");
+            lblColour = new JLabel("Colour:");
+            lblStatus = new JLabel("There are 15 spaces");
+            txtRegNo = new JTextField ();
+            txtColour = new JTextField ();
+            btnAdd = new JButton("Add Car");
+            btnRemove = new JButton("Remove Car");
            
     }
 
@@ -72,7 +65,7 @@ public Week8 () {
            
            constraints.gridy = 1;
            constraints.gridx = 0;
-           this.add(lblColour, constraints);
+           this.add(lblColour, constraints); 
     }
 
 }

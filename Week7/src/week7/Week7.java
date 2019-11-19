b36fdbfaa98fea5ce9726f09c1a5eddbec987812
Casey Littlechild <4littc85@solent.ac.uk>
2019-11-19 16:41:27 +0000

    Finalising Week7 class

--- Week7/src/week7/Week7.java
+++ Week7/src/week7/Week7.java
@@ -13,13 +13,21 @@ public class Week7 {
 
     
     public static void main(String[] args) {
-        Employee em = new Employee("Susan",1200,37,28);
+        Employee em = new Employee("Susan",12500,37,28);
         
-        Manager mg = new Manager("Sam",2750,42,30,19,100);
+        Manager mg = new Manager("Sam",27150,42,30,19,100);
+        
+        FrontLineStaff fs = new FrontLineStaff("Kelly",9,37,30,12);
+        
+        Admin am = new Admin("Jonah",800,37,25,9,7);
         
         System.out.println(em.getDetails());
         
         System.out.println(mg.getDetails());
+        
+        System.out.println(fs.getDetails());
+        
+        System.out.println(am.getDetails());
         
         
         
